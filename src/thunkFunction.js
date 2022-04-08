63edce2af34b42ecd2205f85ff57d546328ab64d
parinz1234 <parinya.ons@gmail.com>
2022-04-08 16:21:30 +0700

    feat: add thunk function

--- src/thunkFunction.js
+++ src/thunkFunction.js
@@ -1,5 +1,40 @@
-import {} from "./fake";
+import { getBooks, createBook } from "./mock-api";
 
 export const fetchBook = () => {
-  return (dispatch, getState) => {};
+  return (dispatch, getState) => {
+    dispatch({
+      type: "FETCH_BOOKS_PENDING",
+    });
+
+    getBooks().then((books) => {
+      dispatch({
+        type: "FETCH_BOOKS_SUCCESS",
+        payload: books,
+      });
+    });
+  };
+};
+
+export const saveBook = (book) => {
+  return (dispatch, getState) => {
+    dispatch({
+      type: "CREATE_BOOK_PENDING",
+    });
+
+    dispatch({
+      type: "FETCH_BOOKS_PENDING",
+    });
+    createBook(book).then(() => {
+      getBooks().then((books) => {
+        dispatch({
+          type: "CREATE_BOOK_SUCCESS",
+        });
+
+        dispatch({
+          type: "FETCH_BOOKS_SUCCESS",
+          payload: books,
+        });
+      });
+    });
+  };
 };
